f4438782ce4f925f07e49448fdf0727683caee8b
Emily EM <eem111@georgetown.edu>
2020-10-19 02:20:48 -0400

    Second Function

---
+++ map_template.R
@@ -0,0 +1,35 @@
+##Assignment 1 Map Plotting Template
+
+library(sp)
+library(rgdal)
+
+clean_data <- read.csv("cleanedSurveyData.csv")
+
+
+plotting_data <- SpatialPoints(clean_data[, c("Longitude", "Latitude")])
+
+#Map of DC neighborhoods from maps2.dcgis.dc.gov
+dc <- readOGR("Georgetown University/Georgetown 2020-2021/Data Analysis with R/Neighborhood_Clusters.shp", "Neighborhood_Clusters")
+
+#Plot the map of DC
+
+par(mar = c(1, 1, 1, 1))
+
+plot(
+  dc,
+  col = "darkgrey",
+  border = "white",
+  main = "District of Columbia Bird Sightings"
+)
+plot(dc[46, ],
+     add = TRUE,
+     col = "#718BAE80",
+     border = "white")
+
+
+#Add your data
+
+plot(plotting_data,
+     add = TRUE,
+     pch = 16,
+     cex = 0.25)
